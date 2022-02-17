322ccb7c92ae3e14f4e81f807c83da859d45f9ba
DESKTOP-7GU4PQQ\Admin <dmitriyEky@yandex.ru>
2022-02-17 23:05:45 +0300

    First commit article page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.main')
+
+@section('title','Заголовок')
+
+@section('pagecontent')
+
+    <main>
+        <section class="main-header">
+            <div class="container">
+                <div class="col-12 bg-light d-inline-block" style="height: 240px;">
+                    <h1>Успех</h1>
+                    <h3>Для молодых и успешных</h3>
+                </div>
+            </div>
+        </section>
+
+        <section class="posts mt-5">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mt-2 col-sm-12">
+                        <div class="card">
+                            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <i class="bi bi-eye-fill"></i>
+                                <a href="#" class="btn btn-primary stretched-link">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+        </section>
+
+
+    </main>
+@endsection
